ad3a2f4e1d1693d96b7d51b89708f9ee9644b5f1
iHoney725 <60086881+ohmyjesus@users.noreply.github.com>
2020-11-07 17:33:20 +0800

    Add files via upload

---
+++ 查找_1顺序查找.cpp
@@ -0,0 +1,29 @@
+﻿//#include <iostream>
+//#include <string>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//
+////顺序查找
+//typedef struct {
+//	int* data;
+//	int tableLen;
+//}SSTable;
+//
+//int search(SSTable ST, int key) {
+//	ST.data[0] = key;
+//	int i;
+//	for (i = ST.tableLen; ST.data[i] != key; i--);  //哨兵机制
+//	return i;
+//}
+//
+//
+//int main()
+//{
+//
+//
+//	cout << "Hello World!" << endl;
+//	return 0;
+//}
+//
---
+++ 查找_2折半查找.cpp
@@ -0,0 +1,40 @@
+//
+//#include <iostream>
+//#include <string>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+////’€∞Î≤È’“
+//typedef struct {
+//	int* data;
+//	int tableLen;
+//}SeqList;
+//
+//int binary_search(SeqList L, int key) {
+//	int low = 0;
+//	int high = L.tableLen - 1;
+//	int mid;
+//	while (low <= high) {
+//		mid = (low + high) / 2;
+//		if (L.data[mid] == key) {
+//			return mid;
+//		}
+//		else if (L.data[mid] > key) {
+//			high = mid - 1;
+//		}
+//		else {
+//			low = mid + 1;
+//		}
+//	}
+//	return -1;
+//}
+//
+//
+//int main()
+//{
+//
+//	cout << "Hello World!" << endl;
+//	return 0;
+//}
+//
